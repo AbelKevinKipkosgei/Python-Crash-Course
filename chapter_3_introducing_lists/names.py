ffd3e20823d66d4bfbe4ebda985d269240a1b676
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-03-29 07:26:58 +0300

    Revised chapter 3.

---
+++ chapter_3_introducing_lists/names.py
@@ -0,0 +1,12 @@
+names = ['Abel', 'Edda', 'Kevin', 'Aryya', 'Tywin']
+print(f"Hello {names[0].title()}, how is your grace?")
+print(f"Hello {names[1].title()}, how is your grace?")
+print(f"Hello {names[2].title()}, how is your grace?")
+print(f"Hello {names[3].title()}, how is your grace?")
+print(f"Hello {names[4].title()}, how is your grace?")
+
+favorite_cars = ['Audi RS7', 'BMW M5 CS', 'Ferrari Purosangue', 'Mercedes Benz AMG S600']
+print(f"My favorite car is {favorite_cars[0]}.")
+print(f"My favorite car is {favorite_cars[1]}.")
+print(f"My favorite car is {favorite_cars[2]}.")
+print(f"My favorite car is {favorite_cars[3]}.")
