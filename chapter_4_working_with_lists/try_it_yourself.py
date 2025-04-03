e3d561b13c256886bdc258c597056b2fb9a5f678
Abel Kevin Kipkosgei <abelkevinkipkosgei>
2025-04-04 01:56:08 +0300

    Worked on try it yourself.

---
+++ chapter_4_working_with_lists/try_it_yourself.py
@@ -0,0 +1,12 @@
+# Pizzas
+pizzas = ['margherita', 'pepperoni', 'neapolitan', 'hawaiian']
+for pizza in pizzas:
+    print(f"\nI like {pizza.title()} pizza.\n")
+print(f"I really love pizza!")
+
+# Animals
+animals = ['dog', 'parrot', 'cat', 'horse']
+for animal in animals:
+    print(f"\nA {animal.lower()} would make a great pet.\n")
+print(f"\nAny of these animals is highly intelligent.\n")
+
