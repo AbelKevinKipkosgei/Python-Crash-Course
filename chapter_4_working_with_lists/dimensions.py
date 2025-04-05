49b65627d3617429f9a99874bc4a9404402c6967
Abel Kevin Kipkosgei <abelkevinkipkosgei>
2025-04-05 23:20:36 +0300

    Worked on dimensions.py.

---
+++ chapter_4_working_with_lists/dimensions.py
@@ -0,0 +1,9 @@
+dimensions = (200, 50)
+print(f"Original dimensions:")
+for dimension in dimensions:
+    print(f"{dimension}")
+
+dimensions = (400, 100)
+print(f"\nModified dimensions:")
+for dimension in dimensions:
+    print(f"{dimension}")
