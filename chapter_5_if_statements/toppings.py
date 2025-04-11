69a69dcae71877c176c50726aa0ef32f192d0ddb
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-11 10:59:37 +0300

    Worked on toppings.py.

--- chapter_5_if_statements/toppings.py
+++ chapter_5_if_statements/toppings.py
@@ -1,3 +1,4 @@
+# Checking for a special item
 requested_toppings = ['mushrooms','green peppers', 'extra cheese']
 for requested_topping in requested_toppings:
     if requested_topping == 'green peppers':
@@ -5,5 +6,24 @@ for requested_topping in requested_toppings:
     else:
         print(f"Adding {requested_topping}")
 
-print(f"\nFinished making your pizza!")
+print(f"Finished making your pizza!")
 
+# Checking that a list is not empty
+requested_toppings = []
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"\nAdding {requested_topping}")
+    print(f"\nFinished making your pizza!")
+else:
+    print(f"\nAre you sure you want a plain pizza?")
+
+# Using multiple lists
+available_toppings = ['mushrooms','olives','green peppers', 'pepperoni',
+                      'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"\nAdding {requested_topping}.")
+    else:
+        print(f"\nSorry, we don't have {requested_topping}.")
+print(f"\nFinished making your pizza!")
