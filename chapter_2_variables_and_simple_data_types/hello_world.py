85dce802b27ddc35e45cc55d74d1daf97e2d38c1
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 11:18:13 +0300

    Worked on hello_world.py.

--- chapter_2_variables_and_simple_data_types/hello_world.py
+++ chapter_2_variables_and_simple_data_types/hello_world.py
@@ -1,3 +1,10 @@
+# # Abel Kevin Kipkosgei, 26th December 2024
+# message = "Hello Python world!"
+# print(message)
+
+# message = "Hello Python Crash Course world!"
+# print(message)
+
 # Abel Kevin Kipkosgei, 26th December 2024
 message = "Hello Python world!"
 print(message)
