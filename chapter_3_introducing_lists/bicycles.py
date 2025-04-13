6dd7c487b1d5b48d334206d9c576adc918d929cc
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 11:27:57 +0300

    Worked on bicycles.py.

--- chapter_3_introducing_lists/bicycles.py
+++ chapter_3_introducing_lists/bicycles.py
@@ -5,7 +5,13 @@
 # message = f"My first bicycle was a {bicycles[2].title()}."
 # print(message)
 
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[1].title())
+# print(bicycles[-1].title())
+# message = f"My first bicycle was a {bicycles[0].title()}."
+# print(message)
+
+bicycles = ["trek", "cannondale", "redline", "specialized"]
 print(bicycles[1].title())
 print(bicycles[-1].title())
 message = f"My first bicycle was a {bicycles[0].title()}."
