a9b86bd7fc3ebb1bb25267e1403e720046c2b381
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 12:17:32 +0300

    Worked on voting.py.

--- chapter_5_if_statements/voting.py
+++ chapter_5_if_statements/voting.py
@@ -1,3 +1,11 @@
+# age = 17
+# if age >= 18:
+#     print(f"\nYou're old enough to vote!")
+#     print(f"\nHave you registered to vote yet?")
+# else:
+#     print(f"\nSorry, you're too young to vote.")
+#     print(f"\nPlease register as soon as you turn 18!")
+
 age = 17
 if age >= 18:
     print(f"\nYou're old enough to vote!")
