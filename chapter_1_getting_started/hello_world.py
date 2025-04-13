2293efe9695a6ba6c8f7160f406b3afd1cf2fb4e
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 11:11:51 +0300

    Worked on hello_world.py.

--- chapter_1_getting_started/hello_world.py
+++ chapter_1_getting_started/hello_world.py
@@ -1,3 +1,11 @@
+# print("Hello Python world!")
+# print("Hello Python world!")
+# message = "Hello Python world!"
+# print(message)
+
+# message = "Hello Python Crash Course world!"
+# print(message)
+
 print("Hello Python world!")
 print("Hello Python world!")
 message = "Hello Python world!"
