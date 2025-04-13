930d25aedb1653069270ed0d8c2676165b37f3f0
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 12:02:35 +0300

    Worked on checking_usernames.py.

--- chapter_5_if_statements/checking_usernames.py
+++ chapter_5_if_statements/checking_usernames.py
@@ -1,5 +1,15 @@
-current_users = ['Abelkevin', 'Johndoe', 'Janewatkins','Laracroft', 'root']
-new_users = ['root','unhinged', 'johndoe','codkiller', 'trex']
+# current_users = ['Abelkevin', 'Johndoe', 'Janewatkins','Laracroft', 'root']
+# new_users = ['root','unhinged', 'johndoe','codkiller', 'trex']
+# current_users_lower = [user.lower() for user in current_users]
+
+# for new_user in new_users:
+#     if new_user.lower() in current_users_lower:
+#         print(f"\nUsername already taken. Please enter a new username.")
+#     else:
+#         print(f"\nUsername is available.")
+
+current_users = ["Abelkevin", "Johndoe", "Janewatkins", "Laracroft", "root"]
+new_users = ["root", "unhinged", "johndoe", "codkiller", "trex"]
 current_users_lower = [user.lower() for user in current_users]
 
 for new_user in new_users:
