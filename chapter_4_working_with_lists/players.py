06f73999f405be093227aabf39b6c3f9b8e03aad
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 11:47:03 +0300

    Worked on players.py.

--- chapter_4_working_with_lists/players.py
+++ chapter_4_working_with_lists/players.py
@@ -1,5 +1,20 @@
+# # Slicing
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(f"\n{players[0:3]}\n")
+# print(f"{players[1:4]}\n")
+# print(f"{players[:4]}\n")
+# print(f"{players[2:]}\n")
+# print(f"{players[-3:]}\n")
+# print(f"{players[::2]}\n")
+# print(f"{players[::-1]}\n")
+
+# # Looping through a slice
+# print(f"Here are the first three players on my team: \n")
+# for player in players[:3]:
+#     print(f"{player.title()}\n")
+
 # Slicing
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
+players = ["charles", "martina", "michael", "florence", "eli"]
 print(f"\n{players[0:3]}\n")
 print(f"{players[1:4]}\n")
 print(f"{players[:4]}\n")
