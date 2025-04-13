98bb01724cc69260f8b4a3cc7df8d75ef98e3d56
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 11:25:58 +0300

    Worked on simple_messages.py.

--- chapter_2_variables_and_simple_data_types/simple_messages.py
+++ chapter_2_variables_and_simple_data_types/simple_messages.py
@@ -1,3 +1,9 @@
+# message = "Python for Data Science."
+# print(message)
+
+# message = "Python for Scripting, Automation and Hacking."
+# print(message)
+
 message = "Python for Data Science."
 print(message)
 
