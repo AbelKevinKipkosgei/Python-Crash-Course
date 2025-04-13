a542121d5723064d19e349c30b5516f25d445b51
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 11:44:05 +0300

    Worked on foods.py.

--- chapter_4_working_with_lists/foods.py
+++ chapter_4_working_with_lists/foods.py
@@ -1,9 +1,25 @@
+# # Copying a List
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# friend_foods = my_foods[:]
+
+# my_foods.append('cannoli')
+# friend_foods.append('ice cream')
+
+# print(f"\nMy favorite foods are: \n")
+# for food in my_foods:
+#     print(f"{food.title()}")
+
+# print(f"\nMy friend's favorite foods are: \n")
+# for food in friend_foods:
+#     print(f"{food.title()}")
+# print("")
+
 # Copying a List
-my_foods = ['pizza', 'falafel', 'carrot cake']
+my_foods = ["pizza", "falafel", "carrot cake"]
 friend_foods = my_foods[:]
 
-my_foods.append('cannoli')
-friend_foods.append('ice cream')
+my_foods.append("cannoli")
+friend_foods.append("ice cream")
 
 print(f"\nMy favorite foods are: \n")
 for food in my_foods:
