8c867a408b0bb30b57b1641aa5b3b2ce3abb677b
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 11:29:15 +0300

    Worked on cars.py.

--- chapter_3_introducing_lists/cars.py
+++ chapter_3_introducing_lists/cars.py
@@ -1,12 +1,36 @@
+# # Sorting a List Permanently with the sort() Method
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort()
+# print(cars)
+# cars.sort(reverse=True)
+# print(cars)
+
+# # Sorting a List Temporarily with the sorted() Function
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print(f"\nHere is the original list: ")
+# print(cars)
+
+# print(f"\nHere is the sorted list: ")
+# print(sorted(cars))
+
+# print(f"\nHere is the original list again: ")
+# print(cars)
+
+# # Printing a List in Reverse Order
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.reverse()
+# print("\nCars in reverse: ")
+# print(cars)
+
 # Sorting a List Permanently with the sort() Method
-cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars = ["bmw", "audi", "toyota", "subaru"]
 cars.sort()
 print(cars)
 cars.sort(reverse=True)
 print(cars)
 
 # Sorting a List Temporarily with the sorted() Function
-cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars = ["bmw", "audi", "toyota", "subaru"]
 print(f"\nHere is the original list: ")
 print(cars)
 
@@ -17,7 +41,7 @@ print(f"\nHere is the original list again: ")
 print(cars)
 
 # Printing a List in Reverse Order
-cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars = ["bmw", "audi", "toyota", "subaru"]
 cars.reverse()
 print("\nCars in reverse: ")
 print(cars)
