7ae5178aa96fde83143a38064c10527ad45ca43f
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-04-13 11:19:38 +0300

    Worked on name_cases.py

--- chapter_2_variables_and_simple_data_types/name_cases.py
+++ chapter_2_variables_and_simple_data_types/name_cases.py
@@ -1,3 +1,33 @@
+# # Name cases exercises to practice variables in Python
+# name = "Abel"
+# message = f"Hello {name}, would you like to learn some Python today?"
+# print(message)
+# full_name = "Abel Kevin Kipkosgei"
+# print(full_name.lower())
+# print(full_name.upper())
+# print(full_name.title())
+# author_name = "Albert Einstein"
+# quote = "A person who has never made a mistake never tried anything new."
+# print(f'{author_name} once said, "{quote}"')
+# famous_person = "Aristotle"
+# famous_person_quote = "Pleasure in the job puts perfection in the work."
+# message = f'{famous_person} once said, "{famous_person_quote}"'
+# print(message)
+# first_name = " Abel "
+# print(f"\n\t'{first_name}'")
+# print(f"\n'{first_name.lstrip()}'")
+# print(f"\n'{first_name.rstrip()}'")
+# print(f"\n'{first_name.strip()}'")
+# file_name = "python_notes.txt"
+# print(f"{file_name.removesuffix(".txt")}")
+# print(5 + 3)
+# print(24 - 16)
+# print(4 * 2)
+# print(64 / 8)
+# favorite_number = 6
+# message = f"My favorite number is: {favorite_number}"
+# print(message)
+
 # Name cases exercises to practice variables in Python
 name = "Abel"
 message = f"Hello {name}, would you like to learn some Python today?"
